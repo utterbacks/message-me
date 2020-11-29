ac3a4a6964c06c6466d22397640530f8a6e8f132
Stephen Utterback <utterbackstephen@gmail.com>
2020-11-29 16:53:43 -0600

    everything before websockets done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Message.create(body: "These are test messages from seeds.", user_id: 1)
+Message.create(body: "These are test messages from seeds.", user_id: 2)
+Message.create(body: "These are test messages from seeds.", user_id: 3)
+Message.create(body: "These are test messages from seeds.", user_id: 4)
+Message.create(body: "These are test messages from seeds.", user_id: 5)

4aab564b0b89c89d13c1508a30594a1bb1f5ff09
Stephen Utterback <utterbackstephen@gmail.com>
2020-11-29 20:40:49 -0600

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id 
+      flash[:success] = "Welcome to MessageMe!"
+      redirect_to root_path
+    else 
+      flash.now[:error] = "Something happened. Try again."
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
